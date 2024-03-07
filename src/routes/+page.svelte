da9231b9fe1737465672a200a5e0718589fbafa2
Mariano Salcedo <mariano1@mit.edu>
2024-03-06 22:32:53 -0500

    svelt_deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>Carlos <b>Mariano</b> Salcedo <b>Salazar</b></h1>
+<p>Welcome! I am a senior at MIT interested in AI and Ethics</p>
+<img class= "home_image" src="images/ai_pic.png" alt="Description of the image">
+
+
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,24 @@
+
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Contact Form</h1>
+
+<form action="mailto:marianosalcedo2001@gmail.com" method="post" enctype="text/plain">
+
+    <label for="email">Email:
+        <input type="email"  name="email" ></label>
+    <br>
+
+    <label for="subject">Subject:
+        <input  name="email" ></label>
+    <br>
+
+    <label for="message">Message:
+        <textarea  name="body" ></textarea></label>
+    <br>
+
+    <button >Submit!</button>
+
+</form>
