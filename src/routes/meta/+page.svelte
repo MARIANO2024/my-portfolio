210130e287e4923a28878b62a9e4635e3aa15ede
Mariano Salcedo <mariano1@mit.edu>
2024-04-03 15:22:27 -0400

    init_plot

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -40,15 +40,29 @@
             enumerable: false,
         });
 
+        // let width = 1000;
+        // let height = 600;
+        
+        // const yScale= d3.scaleLinear().domain(0,24).range(0,height);
+        // const xScale= d2.scaleTime().domain(0,d3.max(ret.date)).range(0,width);
+
         return ret;
 
+        
     
     });
 
-    
+        
     });
 
+    let width = 1000;
+    let height = 600;
+        
+
     
+    const yScale= d3.scaleLinear().domain([0,24]).range([0,height]);
+    let xScale;
+    $: xScale= d3.scaleTime().domain([d3.min(data, d=> d.date),d3.max(data, d=> d.date)]).range([0,width]);
     
 </script>
 
@@ -62,5 +76,33 @@
 
     <dt>Max Depth</dt>
 	<dd>{d3.max(data, d => d.depth)}</dd>
+
+    <dt>Mean Depth</dt>
+	<dd>{d3.mean(data, d => d.depth)}</dd>
+
+    <dt>Number of Files</dt>
+    <dd>{d3.group(data, d => d.file).size}</dd>
 </dl>
+
+
+<svg viewBox="0 0 {width} {height}">
+
+    <g class="dots">
+        {#each commits as commit, index }
+            <circle
+                cx={ xScale(commit.datetime) }
+                cy={ yScale(commit.hourFrac) }
+                r="5"
+                fill="steelblue"
+            />
+        {/each}
+    </g>
+
+</svg>
+
+<style>
+	svg {
+		overflow: visible;
+	}
+</style>
 
